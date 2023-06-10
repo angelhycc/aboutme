5193ed294dcce7e620eef7ae3bc804d768e5d652
angelhyc <angelhyc@umich.edu>
2023-06-10 16:22:29 -0400

    website

---
+++ src/App.js
@@ -0,0 +1,28 @@
+// import logo from './logo.svg';
+import './App.scss';
+import {Routes, Route} from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import LandingPage from './components/LandingPage';
+import GamePage from './components/Game';
+
+
+function App() {
+  return (
+    <>
+    <Routes>
+    {/* <Route path='test' element={<LandingPage/>}/> */}
+    <Route path='/' element={<Layout/>} >
+      <Route index element={<Home />} />
+      <Route path='about' element={<About />}/>
+      {/* <Route path='game' element={<GamePage/>}/> */}
+      <Route path='contact' element={<Contact/>}/>
+    </Route>
+    </Routes>
+    </>
+  );
+}
+
+export default App;
